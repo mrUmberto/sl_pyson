af525242a5646122e6b070b2777bc037140f702b
alexander odegov <kuratorotdiha@gmail.com>
2018-08-20 17:56:58 +0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './lib/pyson.js',
+    externals: [nodeExternals()],
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'pyson.js',
+        libraryTarget: 'umd',
+        library: 'pyson',
+        // Workaround to fix umd build, restore webpack v3 behaviour
+        // https://github.com/webpack/webpack/issues/6677
+        // https://github.com/webpack/webpack/issues/6642
+        globalObject: "typeof self !== 'undefined' ? self : this"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        //new UglifyJsPlugin(),
+    ]
+};
